1152dab43bdd0adcd2413a596932e0be40690fc1
hikarusakura <monoqrock@yahoo.co.jp>
2025-06-13 23:03:38 +0900

    ボタン追加

--- src/components/ScoreUpdater.js
+++ src/components/ScoreUpdater.js
@@ -3,13 +3,9 @@ import React from "react";
 import { db } from "../firebase";
 import { ref, set } from "firebase/database";
 
-export default function ScoreUpdater() {
-  const updateScore = () => {
-    set(ref(db, "scores/teamA"), {
-      runs: 3,
-      hits: 5
-    });
-  };
-
-  return <button onClick={updateScore}>スコアを更新</button>;
-}
+              <button
+                onClick={watchGame}
+                className="px-3 py-1 bg-green-500 hover:bg-green-600 text-white rounded-lg text-xs transition-colors"
+              >
+                観戦画面
+              </button>
